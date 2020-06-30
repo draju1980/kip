694d469b642570519a13a5d8c3d436931b58e71e
Vilmos Nebehaj <v.nebehaj@gmail.com>
2020-06-30 13:21:46 -0700

    Rename GCR and ECR image repos to kip

--- scripts/push_to_gcr_ecr.sh
+++ scripts/push_to_gcr_ecr.sh
@@ -9,6 +9,6 @@ cd $ROOT_DIR
 if [[ $(git describe --dirty) =~ ^v[0-9]+\.[0-9]+\.[0-9]+(-[a-zA-Z0-9]+)$ ]]; then
     gcloud auth configure-docker --quiet
     `aws ecr get-login`
-    make img push-img DKR=docker REGISTRY_REPO=gcr.io/elotl-kip/virtual-kubelet
-    make img push-img DKR=docker REGISTRY_REPO=689494258501.dkr.ecr.us-east-1.amazonaws.com/elotl-kip/virtual-kubelet
+    make img push-img DKR=docker REGISTRY_REPO=gcr.io/elotl-kip/kip
+    make img push-img DKR=docker REGISTRY_REPO=689494258501.dkr.ecr.us-east-1.amazonaws.com/elotl/kip
 fi
